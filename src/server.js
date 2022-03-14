1d285124f70fde72f5eaafa441f75a06d2e6f394
Nathaniel Wills <nathanielwills708@gmail.com>
2022-03-14 10:41:12 -0700

    test

--- src/server.js
+++ src/server.js
@@ -3,12 +3,12 @@ const { PORT = 5000 } = process.env;
 const app = require("./app");
 const knex = require("./db/connection");
 
-app.listen(PORT);
+//app.listen(PORT);
 
-// knex.migrate
-//   .latest()
-//   .then((migrations) => {
-//     console.log("migrations", migrations);
-//     app.listen(PORT, listener);
-//   })
-//   .catch(console.error);
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("migrations", migrations);
+    app.listen(PORT);
+  })
+  .catch(console.error);
